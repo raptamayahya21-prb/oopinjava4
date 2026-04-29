50e5d7653bc0b544213cda5cd590454eb2d7bd04
raptamayahya21-prb <raptamayahya21@gmail.com>
2026-04-29 22:19:57 +0700

    Add files via upload

---
+++ Manusia.java
@@ -0,0 +1,6 @@
+public class Manusia implements BisaBergerak {
+    @Override
+    public void bergerak() {
+        System.out.println("Manusia bergerak dengan berjalan.");
+    }
+}
---
+++ Matematika.java
@@ -0,0 +1,13 @@
+public class Matematika {
+    public int kali(int a, int b) {
+        return a * b;
+    }
+
+    public double kali(double a, double b) {
+        return a * b;
+    }
+
+    public int kali(int a, int b, int c) {
+        return a * b * c;
+    }
+}
