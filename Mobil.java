3583f201c6abd68b9bf1ba05ae750bbdba8c9939
raptamayahya21-prb <raptamayahya21@gmail.com>
2026-04-29 22:21:13 +0700

    Add files via upload

---
+++ Mobil.java
@@ -0,0 +1,6 @@
+public class Mobil implements BisaBergerak {
+    @Override
+    public void bergerak() {
+        System.out.println("Mobil bergerak menggunakan roda.");
+    }
+}
---
+++ PesawatKargo.java
@@ -0,0 +1,6 @@
+public class PesawatKargo extends Kendaraan implements BisaMengangkut {
+    @Override
+    public void angkut() {
+        System.out.println("Pesawat Kargo mengangkut barang logistik.");
+    }
+}
